5b2235b3e0df58b8f3d685cfbca6f22a5aeb1e42
dowin <490593627@qq.com>
2024-08-09 14:52:33 +0800

    7

--- aiqt-data/aws_okx_Websocket2.py
+++ aiqt-data/aws_okx_Websocket2.py
@@ -6,18 +6,26 @@ from okx.MarketData import MarketAPI
 from okx.Account import AccountAPI
 
 
+
+
+
+
 # 用你的API密钥替换下面的'YOUR_API_KEY'和'YOUR_API_SECRET'
 api_key = "7f27a0b4-ceb3-4e4f-bcf1-f33ce44854d4" # "Aiqt@2024"
 
 api_secret = "3430205DBFB07579E48EE2063FDB02A0"
 
-# 创建Account和Order对象
-account = AccountAPI(api_key, api_secret)
-#order = Order(api_key, api_secret)
 
-# 获取账户余额
 
-balance = account.get_account_balance()
-print(balance)
+# 初始化Market对象
+market = MarketAPI()
+
+# 获取交易对列表
+symbols = market.get_symbols()
+
+# 查找BTC-USDT交易对的交易对ID
+for symbol in symbols:
+    if symbol['instId'] == 'BTC-USDT':
+        print(f"BTC-USDT的交易对ID为: {symbol['instId']}")
 
 
