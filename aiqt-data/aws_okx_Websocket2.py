65e0fe1bc7688521a0f5f29b2043c60d964e8ca3
dowin <490593627@qq.com>
2024-08-09 17:16:02 +0800

    20

--- aiqt-data/aws_okx_Websocket2.py
+++ aiqt-data/aws_okx_Websocket2.py
@@ -9,11 +9,28 @@ from okx.Account import AccountAPI
 from okx.okxclient import OkxClient
 from config import API_KEY, API_SECRET, PASSPHRASE
 
+
+
+
+import okx.PublicData as PublicData
+
+flag = "0"  # 实盘:0 , 模拟盘：1
+
+publicDataAPI = PublicData.PublicAPI(flag=flag)
+
+# 获取交易产品基础信息
+result = publicDataAPI.get_instruments(
+    instType="SPOT"
+)
+print(result)
+
+
+
 # 创建一个OkxClient实例
 #子账户
 #client = OkxClient(api_key="7f27a0b4-ceb3-4e4f-bcf1-f33ce44854d4", api_secret_key="3430205DBFB07579E48EE2063FDB02A0", passphrase="Aiqt@2024")
 #主账户
-client = OkxClient(api_key="f2b08d9f-35c9-4b5d-b3bb-12f00b82a52d", api_secret_key="7CC4B437BAFAE692E7EEC9449FB54FAA", passphrase="Aiqt@2024")
+#client = OkxClient(api_key="f2b08d9f-35c9-4b5d-b3bb-12f00b82a52d", api_secret_key="7CC4B437BAFAE692E7EEC9449FB54FAA", passphrase="Aiqt@2024")
 
 # 
 # client = OkxClient(
@@ -37,6 +54,6 @@ client = OkxClient(api_key="f2b08d9f-35c9-4b5d-b3bb-12f00b82a52d", api_secret_ke
 # print(ticker)
 # 
 
-account = AccountAPI(client)
-response = account.get_account_balance()
-print(response)
+#account = AccountAPI(client)
+#response = account.get_account_balance()
+#print(response)
