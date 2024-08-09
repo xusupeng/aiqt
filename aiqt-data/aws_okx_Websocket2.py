8c111266a3f847f831a8767d2d037999804ae17f
dowin <490593627@qq.com>
2024-08-09 14:24:39 +0800

    5

--- aiqt-data/aws_okx_Websocket2.py
+++ aiqt-data/aws_okx_Websocket2.py
@@ -6,29 +6,18 @@ from okx.MarketData import MarketAPI
 from okx.Account import AccountAPI
 
 
-# 安装websocket库和pythonSDK库：pip install websocket-client python_okx
-# 创建API对象
+# 用你的API密钥替换下面的'YOUR_API_KEY'和'YOUR_API_SECRET'
+api_key = "7f27a0b4-ceb3-4e4f-bcf1-f33ce44854d4" # "Aiqt@2024"
 
-market_api = MarketAPI("7f27a0b4-ceb3-4e4f-bcf1-f33ce44854d4", "3430205DBFB07579E48EE2063FDB02A0", "Aiqt@2024", False)
+api_secret = "3430205DBFB07579E48EE2063FDB02A0"
 
-def on_message(ws, message):
-    print(message)
+# 创建Account和Order对象
+account = AccountAPI(api_key, api_secret)
+#order = Order(api_key, api_secret)
 
-def on_error(ws, error):
-    print(error)
+# 获取账户余额
 
-def on_close(ws, close_status_code, close_msg):
-    print("连接已关闭", close_status_code, close_msg)
+balance = account.get_balance()
+print(balance)
 
-def on_open(ws):
-    print("连接已打开")
-
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://wsaws.okx.com:8443/ws/v5/public",
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
 
