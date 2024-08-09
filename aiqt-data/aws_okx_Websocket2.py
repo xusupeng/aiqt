7c0289d4984623109f7bb43730fb57035f7db492
dowin <490593627@qq.com>
2024-08-09 15:16:58 +0800

    8

--- aiqt-data/aws_okx_Websocket2.py
+++ aiqt-data/aws_okx_Websocket2.py
@@ -4,28 +4,16 @@ import sys
 sys.path.append('/aiqt/myenv/lib/python3.12/site-packages/okx/')
 from okx.MarketData import MarketAPI
 from okx.Account import AccountAPI
+from config import API_KEY, API_SECRET, PASSPHRASE
 
+# 创建一个OkxClient实例
+client = OkxClient(api_key=API_KEY, api_secret=API_SECRET, passphrase=PASSPHRASE)
 
+# 创建一个MarketAPI实例
+market = MarketAPI(client)
 
+# 获取指定交易对的最新价格信息
+ticker = market.get_ticker(instId='BTC-USDT')
 
-
-
-# 用你的API密钥替换下面的'YOUR_API_KEY'和'YOUR_API_SECRET'
-api_key = "7f27a0b4-ceb3-4e4f-bcf1-f33ce44854d4" # "Aiqt@2024"
-
-api_secret = "3430205DBFB07579E48EE2063FDB02A0"
-
-
-
-# 初始化Market对象
-market = MarketAPI()
-
-# 获取交易对列表
-symbols = market.get_symbols()
-
-# 查找BTC-USDT交易对的交易对ID
-for symbol in symbols:
-    if symbol['instId'] == 'BTC-USDT':
-        print(f"BTC-USDT的交易对ID为: {symbol['instId']}")
-
-
+# 打印结果
+print(ticker)
