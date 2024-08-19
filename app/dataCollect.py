eec591fd0d41efba4f460311f5d971a29bf7994a
dowin <490593627@qq.com>
2024-08-19 16:22:36 +0800

    xxxx

--- app/dataCollect.py
+++ app/dataCollect.py
@@ -11,23 +11,24 @@ import okx.MarketData as MarketData
 
 flag = "0"  # 实盘:0 , 模拟盘:1
 
-publicDataAPI = PublicData.PublicAPI(flag=flag)
+#publicDataAPI = PublicData.PublicAPI(flag=flag)
 # 获取交易产品基础信息  
 # 产品类型 SPOT：币币   MARGIN：币币杠杆   SWAP：永续合约   FUTURES：交割合约   OPTION：期权
-result = publicDataAPI.get_instruments(instType="SWAP")
+
+#result = publicDataAPI.get_instruments(instType="SWAP")
 #print("获取交易产品基础信息(SWAP永续合约)：%s" % result)
-for i, item in enumerate(result['data']):
-    print("获取交易产品基础信息(SWAP永续合约%s)：%s" % (i, item))
-    if i >= 5:  break
+#for i, item in enumerate(result['data']):
+#    print("获取交易产品基础信息(SWAP永续合约%s)：%s" % (i, item))
+#    if i >= 5:  break
 
 # 获取持仓总量
 marketDataAPI =  MarketData.MarketAPI(flag=flag)
 # 获取所有指数行情
 try:
     result = marketDataAPI.get_index_tickers(instId="ETH-USD")
-    print("获取所有指数行情:ETH-USD:%s" % result)
+    print("获取行情:ETH-USD:%s" % result)
 except Exception as e:
-    print("获取所有指数行情:ETH-USD:%s" % e)
+    print("获取行情:ETH-USD:%s" % e)
 
 
 
