c5e9385d3100e4ebc8e288b235263938325f364d
dowin <490593627@qq.com>
2024-08-20 14:54:36 +0800

    cccc

--- app/dataCollect.py
+++ app/dataCollect.py
@@ -13,15 +13,15 @@ import okx.MarketData as MarketData
 
 flag = "0"  # 实盘:0 , 模拟盘:1
 
-#publicDataAPI = PublicData.PublicAPI(flag=flag)
+publicDataAPI = PublicData.PublicAPI(flag=flag)
 # 获取交易产品基础信息  
 # 产品类型 SPOT：币币   MARGIN：币币杠杆   SWAP：永续合约   FUTURES：交割合约   OPTION：期权
 
-#result = publicDataAPI.get_instruments(instType="SWAP")
-#print("获取交易产品基础信息(SWAP永续合约)：%s" % result)
-#for i, item in enumerate(result['data']):
-#    print("获取交易产品基础信息(SWAP永续合约%s)：%s" % (i, item))
-#    if i >= 5:  break
+result = publicDataAPI.get_instruments(instType="SWAP")
+print("获取交易产品基础信息(SWAP永续合约)：%s" % result)
+for i, item in enumerate(result['data']):
+    print("获取交易产品基础信息(SWAP永续合约%s)：%s" % (i, item))
+    if i >= 5:  break
 
 def dataCollect():
     # 获取持仓总量
@@ -38,10 +38,10 @@ def dataCollect():
 
 
 # 查看账户余额
-# accountAPI = Account.AccountAPI(API_KEY, API_SECRET, PASSPHRASE, False, flag)
-# result = accountAPI.get_account_balance()
-# print("查看账户余额")
-# print(result)
+accountAPI = Account.AccountAPI(API_KEY, API_SECRET, PASSPHRASE, False, flag)
+result = accountAPI.get_account_balance()
+print("查看账户余额")
+print(result)
 
 
 
