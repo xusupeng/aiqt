93b5bc2452615afc78043dde13201c12045608c9
dowin <490593627@qq.com>
2024-08-20 10:44:37 +0800

    比比皆是

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-if __name__ == "__main__":
-    import os
-    import sys
-    sys.path.append('./aiqtEnv/lib/python3.12/site-packages/')
-    from typing import Union
-    from fastapi import FastAPI
-    #from app.dataCollect import dataCollect
 
-    app = FastAPI()
+import os
+import sys
+sys.path.append('./aiqtEnv/lib/python3.12/site-packages/')
+from typing import Union
+from fastapi import FastAPI
+#from app.dataCollect import dataCollect
 
-    @app.get("/")
-    async def hello():
-        return {"message": "Hello, AIQT!"}
+app = FastAPI()
 
-    #@app.get("/items/{item_id}")
-    #async def read_item(item_id: int, q: Union[str, None] = None):
-    #    return {"item_id": item_id, "q": q}
+@app.get("/")
+async def hello():
+    return {"message": "Hello, AIQT!"}
 
-    #@app.get("/dataCollect")
-    #async def dataCollect():
-    #    dataCollect()
-    #    return {"message": "dataCollect!"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
-    os.environ["aiqtEnv"] = "production"
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#@app.get("/dataCollect")
+#async def dataCollect():
+#    dataCollect()
+#    return {"message": "dataCollect!"}
